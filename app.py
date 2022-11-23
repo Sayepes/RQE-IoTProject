5ade1513c45737e6eb6f935040d3af3251273071
Carstal <43147122+Carstal@users.noreply.github.com>
2022-11-23 10:39:06 -0500

    Planifying post request

--- app.py
+++ app.py
@@ -1,7 +1,29 @@
-from flask import Flask
-
+import pymongo as pymongo
+from flask import Flask, request, jsonify
+# from flask_objectid_converter import ObjectIDConverter
+from pymongo import ReturnDocument
+from pymongo.server_api import ServerApi
+# from Schemas import ToDoItemSchemaPut, ToDoItemSchemaPost, ToDoItemsSchemaPatch
+from bson import json_util, ObjectId
+# from flask_cors import CORS
 app = Flask(__name__)
 
+#New reading from arduino
+@app.route('/read', methods=["POST"])
+def add_new_reading():
+    # Get JSON Object
+    reading = request.json
+    #Validate JSON Object
+    #Schema Validation
+    error = False
+    if error:
+        return error, 400
+    #Parse JSON object
+
+    #Write to DB
+
+    #Return Success
+    return "Complete"
 
 @app.route('/')
 def hello_world():  # put application's code here
