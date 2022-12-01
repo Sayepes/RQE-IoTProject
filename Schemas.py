d2304b06b7bbd0ce7c7d1cb5d12ead9e0ab2c2c7
Carstal <43147122+Carstal@users.noreply.github.com>
2022-11-30 19:45:51 -0500

    Fixed Schema Validation Issue

--- Schemas.py
+++ Schemas.py
@@ -3,9 +3,9 @@ from marshmallow import Schema, fields, validate
 class ReadingSchemaPost(Schema):
     collection_id = fields.String(required=True)
     time = fields.DateTime(required=True)
-    temp = fields.Double(required=True)
-    humi = fields.Double(required=True)
-    lumi = fields.Double(required=True)
+    temp = fields.Float(required=True)
+    humi = fields.Float(required=True)
+    lumi = fields.Float(required=True)
 
 
 
